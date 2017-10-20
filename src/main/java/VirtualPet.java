94cf2b51ba626e79aab6c42eafd85b410f928185
Sarah Strange <strange1120@gmail.com>
2017-10-20 09:54:02 -0400

    first commit friday

---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,57 @@
+
+public class VirtualPet {
+
+	protected int health;
+	protected int happiness;
+	protected String name;
+	protected String description;
+	protected int boredom;
+	protected int cleanliness;
+
+	public VirtualPet(int health, int happiness, String name, String description, int boredom, int cleanliness) {
+		this.health = health;
+		this.happiness = happiness;
+		this.name = name;
+		this.description = description;
+		this.boredom = boredom;
+		this.cleanliness = cleanliness;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public int getHappiness() {
+		return happiness;
+	}
+
+	public int getBoredom() {
+		return boredom;
+	}
+
+	public int getCleanliness() {
+		return cleanliness;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Name: " + name + "Health: " + health + "Happiness: " + happiness + "Boredom: " + boredom + "Cleanliness: " + cleanliness + 
+				"\nDescription: " + description;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/VirtualPetShelter.java
@@ -0,0 +1,9 @@
+
+public class VirtualPetShelter {
+
+	
+	public void showPetStats() {
+		for(VirtualPet pet: pets) {
+			System.out.println(pet);
+		}
+}
